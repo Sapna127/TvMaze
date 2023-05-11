b5aea85e2eadaca6cc12be6678464f9fc209ae27
Sapna Kul <kulsapna2210002@gmail.com>
2023-05-11 18:36:03 +0530

    added the css files

--- tvmaze/src/components/ShowDeatails/ShowDetails.jsx
+++ tvmaze/src/components/ShowDeatails/ShowDetails.jsx
@@ -1,44 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import axios from 'axios';
-import './ShowDetails.css'; // Import the CSS file
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import "./ShowDetails.css"; // Import the CSS file
 
 const ShowDetails = () => {
   const location = useLocation();
-  const id = location.pathname.split('/').pop();
+  const id = location.pathname.split("/").pop();
   const [show, setShow] = useState(null);
 
   useEffect(() => {
     axios
       .get(`https://api.tvmaze.com/shows/${id}`)
-      .then(response => {
+      .then((response) => {
         const showDetails = response.data;
         setShow(showDetails);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, [id]);
 
+  useEffect(() => {
+    if (show && show.image && show.image.medium) {
+      document.body.style.background = `url(${show.image.medium}) no-repeat center center fixed`;
+      document.body.style.backgroundSize = "cover";
+    }
+    return () => {
+      document.body.style.background = "";
+    };
+  }, [show]);
+
   return (
-    <div className="show-details">
-      {show ? (
-        <div className="container">
-          <div className="movie-info">
-            <h1 className="title">{show.name}</h1>
-            <img src={show.image.medium} alt={show.name} className="movie-img" />
-            <p className="summary">{show.summary}</p>
-            <div className="details">
-              <p className="rating">{show.rating.average || 'N/A'}</p>
-              <p className="year">{show.premiered}</p>
-              <p className="genre">{show.genres.join(' | ')}</p>
-              <p className="time">{show.runtime || 'N/A'}</p>
+    <div className="whole">
+      <div></div>
+      <div className="show-details">
+        {show ? (
+          <div className="wrapper" key={show.id}>
+            <div className="main_card">
+              <div className="card_left">
+                <div className="card_details">
+                  <h1>{show.name}</h1>
+                  <div className="card_cat">
+                    <p className="PG">{show.rating?.average || "N/A"}</p>
+                    <p className="year">{show.premiered}</p>
+                    <p className="genre">{show.genres?.join(" | ")}</p>
+                    <p className="time">{show.runtime || "N/A"}</p>
+                  </div>
+                  <p className="disc">{show.summary}</p>
+
+                  <div className="social-btn">
+                    <a
+                      href={show.officialSite}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button>WATCH NOW</button>
+                    </a>
+
+                    {/* GET */}
+                    <button>DOWNLOAD</button>
+                  </div>
+                </div>
+              </div>
             </div>
-            <a href={show.officialSite} target="_blank" rel="noopener noreferrer" className="watch-btn">
-            Watch</a>
+            <div className="img_container">
+              {/* Render show image here if needed */}
+            </div>
           </div>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 };
